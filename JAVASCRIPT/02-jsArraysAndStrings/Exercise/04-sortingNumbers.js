857e441898018fd7d66ccd1bfc51fa012c491d4c
GeorgiDN <go6odn28@gmail.com>
2024-09-27 18:42:23 +0300

    added more solutions

--- JAVASCRIPT/02-jsArraysAndStrings/Exercise/04-sortingNumbers.js
+++ JAVASCRIPT/02-jsArraysAndStrings/Exercise/04-sortingNumbers.js
@@ -1,19 +1,15 @@
 function sorNumbers(arr) {
     let sortedArray = arr.sort((a, b) => a - b);
-    let firstHalfArray = sortedArray.slice(0, arr.length / 2);
-    let secondHalfArray = sortedArray.slice();
-    let iterations = arr.length / 2
+    let iterations = arr.length / 2;
     let newArray = [];
 
     for (let i = 0; i < iterations; i++) {
-        if (firstHalfArray.length !== 0) {
-        newArray.push(firstHalfArray.shift());
-        }
-        if (secondHalfArray.length !== 0) {
-        newArray.push(secondHalfArray.pop())
+        if (sortedArray.length !== 0) {
+        newArray.push(sortedArray.shift());
+        newArray.push(sortedArray.pop());
         }
     }
-    return newArray
+    return newArray;
 }
 
 console.log(sorNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
--- JAVASCRIPT/02-jsArraysAndStrings/Exercise/06-modernTimesOfHashTag.js
+++ JAVASCRIPT/02-jsArraysAndStrings/Exercise/06-modernTimesOfHashTag.js
@@ -1,14 +1,26 @@
 function findSpecialWords(string) {
-    let stringArray = string.split(" ");
-    let specialWords = [];
-    let regexPattern = /#[A-Za-z]/;
-
-    for (let word of stringArray) {
-        if (word.match(regexPattern)) {
-            specialWords.push(word.slice(1));
+    for (let word of string.split(" ")) {
+        if (word.match(/#[A-z]/)) {
+            console.log(word.slice(1));
         }
     }
-    console.log(specialWords.join("\n"));
 }
+
+
+// function findSpecialWords(string) {
+//     let stringArray = string.split(" ");
+//     let specialWords = [];
+//     let regexPattern = /#[A-z]/;
+//     // let regexPattern = /#[A-Za-z]/;
+//
+//     for (let word of stringArray) {
+//         if (word.match(regexPattern)) {
+//             specialWords.push(word.slice(1));
+//         }
+//     }
+//     console.log(specialWords.join("\n"));
+// }
+//
+
 
 findSpecialWords('Nowadays everyone uses # to tag a #special word in #socialMedia')

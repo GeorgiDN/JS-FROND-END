7f57c9a1338cd0a92d816e5d5375990653ffb3c8
GeorgiDN <go6odn28@gmail.com>
2024-09-28 14:15:14 +0300

    added more solutions

---
+++ JAVASCRIPT/03-functionsAndStatements/03.Functions-and-Statements-Demo/02-mathPower.js
@@ -0,0 +1,16 @@
+function mathPower(num, power) {
+    if (power === 1) {
+        return num
+    }
+    return num * mathPower(num, power - 1);
+}
+
+console.log(mathPower(2, 8));
+
+
+
+// function mathPower(num, power) {
+//     console.log(num ** power);
+// }
+//
+// mathPower(2, 8);

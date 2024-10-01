1d707971cec551f0c51c9d9c2ecd379e91274352
GeorgiDN <go6odn28@gmail.com>
2024-10-01 08:03:55 +0300

    added more solutions

---
+++ JAVASCRIPT/04-objectAndClasses/Demo/04.Objects-and-Classes/1-person-info.js
@@ -0,0 +1,14 @@
+function solve(firstName, lastName, age) {
+    let info = {
+        firstName,
+        lastName,
+        age,
+    };
+
+    return info;
+}
+
+console.log(solve("Peter",
+"Pan",
+"20"
+));

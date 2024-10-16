b19f616c7ccee4256625947f2c454deb2598f044
GeorgiDN <go6odn28@gmail.com>
2024-10-16 21:50:12 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Lab/02.Collect List Items/CollectListItems.js
+++ JAVASCRIPT/05-domIntorduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,18 @@
 function extractText() {
-    // TODO
+    const ulElement = document.getElementById('items');
+    const resultElement = document.getElementById('result');
+    const textResult = ulElement.innerText;
+
+    resultElement.textContent = textResult;
 }
+
+
+// function extractText() {
+//     let  textToExtract = Array.from(document.getElementsByTagName("li"));
+//
+//     let text = textToExtract.map( element => {
+//         return element.textContent
+//     }).join("\n");
+//
+//     document.getElementById("result").textContent = text;
+// }

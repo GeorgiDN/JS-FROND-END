cd56fa3a7cce99912ea9770897304240ca216bb2
GeorgiDN <go6odn28@gmail.com>
2024-10-17 09:35:12 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Lab/demo/extractText/ExtractParenthesis.js
+++ JAVASCRIPT/05-domIntorduction/Lab/demo/extractText/ExtractParenthesis.js
@@ -2,6 +2,6 @@ function extract(content) {
     const contentElement = document.getElementById(content);
     const pattern = /\(([^()]+)\)/g;
     const matches = contentElement.textContent.matchAll(pattern);
-    const result = Array.from(matches).map(match => match[1]).join(';');
-    return result;
+    const result = Array.from(matches).map(match => match[1]).join('; ');
+    document.getElementById('output').textContent = result; // Display the result in the <p> element
 }

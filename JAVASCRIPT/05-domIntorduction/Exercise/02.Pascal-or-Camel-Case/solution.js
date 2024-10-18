ddd456c3c09427c35d0d9273f46d0f5525b4d5a5
GeorgiDN <go6odn28@gmail.com>
2024-10-18 21:15:31 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/02.Pascal-or-Camel-Case/solution.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/02.Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,23 @@
-function solve() {
-  //TODO...
+function solve () {
+    const validNamingConventions = ["Camel Case", "Pascal Case"];
+    const textInputEl = document.querySelector("#text");
+    const namingConventionEl = document.querySelector("#naming-convention");
+    const resultEl = document.querySelector(".result-container #result");
+
+    if (!validNamingConventions.includes(namingConventionEl.value)) {
+        resultEl.textContent = "Error!";
+        return;
+    }
+
+    const words = textInputEl.value.toLowerCase().split(" ");
+    let result = "";
+
+    if (namingConventionEl.value === "Camel Case") {
+        result = words[0] + words.slice(1)
+            .map(word => word[0].toUpperCase() + word.slice(1))
+            .join("")
+    } else if (namingConventionEl.value === "Pascal Case") {
+        result = words.map(word => word[0].toUpperCase() + word.slice(1)).join("");
+    }
+    resultEl.textContent = result;
 }

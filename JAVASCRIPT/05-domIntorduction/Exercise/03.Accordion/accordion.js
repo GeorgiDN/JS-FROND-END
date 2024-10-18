f003b5e0cfb1e436c8594407cb1eb272b6a782ba
GeorgiDN <go6odn28@gmail.com>
2024-10-18 22:27:54 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/03.Accordion/accordion.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/03.Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttonEl = document.getElementsByClassName("button")[0];
+    const extraEl = document.getElementById("extra");
+    const isHidden = extraEl.style.display === "none";
+
+    if (isHidden || !extraEl.style.display) {
+        extraEl.style.display = "block";
+        buttonEl.textContent = "Less";
+    } else {
+        extraEl.style.display = "none";
+        buttonEl.textContent = "More";
+    }
 }

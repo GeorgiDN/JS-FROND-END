cd7af0f1e37ac90ca10ecaef325e17b43640834c
GeorgiDN <go6odn28@gmail.com>
2024-10-19 09:38:20 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/05.Table-Search-Engine/solution.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/05.Table-Search-Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        const tableRowEls = document.querySelectorAll("table.container tbody tr");
+        const searchedText = document.getElementById("searchField").value.toLowerCase();
 
-   }
+        tableRowEls.forEach((row) => {
+            row.classList.remove("select");
+        });
+
+        tableRowEls.forEach((row) => {
+            if (searchedText.length > 0 && row.textContent.toLowerCase().includes(searchedText)) {
+                row.classList.add("select");
+            }
+        });
+        document.getElementById("searchField").value = "";
+    }
 }

6031e312636edd2f28425c79bd4c4588316bc4c5
GeorgiDN <go6odn28@gmail.com>
2024-10-19 09:43:30 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/05.Table-Search-Engine/solution.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/05.Table-Search-Engine/solution.js
@@ -5,15 +5,43 @@ function solve() {
         const tableRowEls = document.querySelectorAll("table.container tbody tr");
         const searchedText = document.getElementById("searchField").value.toLowerCase();
 
-        tableRowEls.forEach((row) => {
-            row.classList.remove("select");
-        });
+        function removeClassList(tableRowEls) {
+            tableRowEls.forEach((row) => {
+                row.classList.remove("select");
+            });
+        }
 
-        tableRowEls.forEach((row) => {
-            if (searchedText.length > 0 && row.textContent.toLowerCase().includes(searchedText)) {
-                row.classList.add("select");
-            }
-        });
+        function addClassList(tableRowEls) {
+            tableRowEls.forEach((row) => {
+                if (searchedText.length > 0 && row.textContent.toLowerCase().includes(searchedText)) {
+                    row.classList.add("select");
+                }
+            });
+        }
+
+        removeClassList(tableRowEls);
+        addClassList(tableRowEls);
         document.getElementById("searchField").value = "";
     }
 }
+
+
+// function solve() {
+//     document.querySelector('#searchBtn').addEventListener('click', onClick);
+//
+//     function onClick() {
+//         const tableRowEls = document.querySelectorAll("table.container tbody tr");
+//         const searchedText = document.getElementById("searchField").value.toLowerCase();
+//
+//         tableRowEls.forEach((row) => {
+//             row.classList.remove("select");
+//         });
+//
+//         tableRowEls.forEach((row) => {
+//             if (searchedText.length > 0 && row.textContent.toLowerCase().includes(searchedText)) {
+//                 row.classList.add("select");
+//             }
+//         });
+//         document.getElementById("searchField").value = "";
+//     }
+// }

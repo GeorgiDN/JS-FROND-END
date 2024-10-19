f9c223100df64fc92f7e20f7d6dd5ae51ec4077b
GeorgiDN <go6odn28@gmail.com>
2024-10-19 08:47:47 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/04.Search-in-List/search.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/04.Search-in-List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+    const searchText  = document.getElementById("searchText").value.toLowerCase();
+    const resultEl = document.getElementById("result");
+    const towns = document.querySelectorAll("#towns li");
+    let matches = [];
+
+    towns.forEach((town) => {
+        town.style.fontWeight = "normal";
+        town.style.textDecoration = "none";
+    });
+
+    towns.forEach((town) => {
+        if (searchText.length > 0 && town.textContent.toLowerCase().includes(searchText)) {
+            town.style.fontWeight = "bold";
+            town.style.textDecoration = "underline";
+            matches.push(town.textContent);
+        }
+    })
+    resultEl.textContent = `${matches.length} matches found`;
 }

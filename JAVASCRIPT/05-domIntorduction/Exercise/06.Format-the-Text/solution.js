eb3b19b4ac43991d8b54aee99cfc624c5741b5c2
GeorgiDN <go6odn28@gmail.com>
2024-10-19 15:00:05 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/06.Format-the-Text/solution.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/06.Format-the-Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+    const inputEl = document.getElementById("input").value.trim();
+    const outputEl = document.getElementById("output");
+    let allSentences = inputEl.split(".").filter(Boolean).map(sentence => sentence.trim());
+
+    for (let i = 0; i < allSentences.length; i += 3) {
+        let sentenceGroup = allSentences.slice(i, i + 3).join(".").concat(".");
+        let paragraphEl = document.createElement("p");
+        paragraphEl.textContent = sentenceGroup;
+        outputEl.appendChild(paragraphEl);
+    }
 }

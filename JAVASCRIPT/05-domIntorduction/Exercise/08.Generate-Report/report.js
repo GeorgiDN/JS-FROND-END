70075a823fe559e028f94cb6138506a5ccd0329a
GeorgiDN <go6odn28@gmail.com>
2024-10-22 09:37:33 +0300

    added more solutions

--- JAVASCRIPT/05-domIntorduction/Exercise/08.Generate-Report/report.js
+++ JAVASCRIPT/05-domIntorduction/Exercise/08.Generate-Report/report.js
@@ -1,26 +1,25 @@
 function generateReport() {
     const checkboxes = document.querySelectorAll("th input[type='checkbox']");
-    const table = document.querySelector("table");
-    const rows = document.querySelectorAll("tbody tr");
-     let textAreaEl = document.getElementById("output");
-     let result = [];
-     let selectedColumns = [];
+    const rowsEl = document.querySelectorAll("tbody tr");
+    let textAreaEl = document.getElementById("output");
+    let result = [];
+    let selectedColumns = [];
 
-     checkboxes.forEach((checkbox, index) => {
-         if (checkbox.checked) {
-             selectedColumns.push({
-                 name: checkbox.name,
-                 index: index
-             });
-         }
-     });
+    checkboxes.forEach((checkbox, index) => {
+        if (checkbox.checked) {
+            selectedColumns.push({
+                name: checkbox.name,
+                index: index
+            });
+        }
+    });
 
-     rows.forEach((row) => {
-         let rowObject = {};
-         selectedColumns.forEach((column) => {
-             rowObject[column.name] = row.cells[column.index].textContent.trim();
-         });
-         result.push(rowObject);
-     });
+    rowsEl.forEach((row) => {
+        let rowObject = {};
+        selectedColumns.forEach((column) => {
+            rowObject[column.name] = row.cells[column.index].textContent.trim();
+        });
+        result.push(rowObject);
+    });
     textAreaEl.value = JSON.stringify(result, null, 2);
 }

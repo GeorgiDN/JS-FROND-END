f009be086398ccb69eb6cc91ff6e899d47188957
GeorgiDN <go6odn28@gmail.com>
2024-10-27 18:55:10 +0200

    added more solutions

--- JAVASCRIPT/06-domManipulationEvents/Lab/04. Mouse-Gradient/app.js
+++ JAVASCRIPT/06-domManipulationEvents/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const resulEl = document.getElementById("result");
+    const gradientEl = document.getElementById("gradient");
+
+    gradientEl.addEventListener("mousemove", (e) => {
+        const currentPosition = e.offsetX;
+        const elementWidth = e.currentTarget.clientWidth;
+        let percent = (currentPosition / elementWidth * 100);
+
+        if (percent > 99.5) {
+            percent = 100;
+        } else {
+            percent = Math.floor(percent)
+        }
+        resulEl.textContent = `${percent}%`;
+    });
 }

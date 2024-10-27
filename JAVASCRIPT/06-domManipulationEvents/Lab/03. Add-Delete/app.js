bd91985e11d421ae974d1630cbf9e67441c80732
GeorgiDN <go6odn28@gmail.com>
2024-10-27 18:24:10 +0200

    added more solutions

--- JAVASCRIPT/06-domManipulationEvents/Lab/03. Add-Delete/app.js
+++ JAVASCRIPT/06-domManipulationEvents/Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    debugger
+    const itemsElement = document.getElementById("items");
+    const inputEl = document.getElementById("newItemText");
+    const itemText = inputEl.value;
+    const liElement = document.createElement("li");
+    liElement.textContent = itemText;
+    itemsElement.appendChild(liElement);
+
+    const deleteButton = document.createElement("a");
+    deleteButton.textContent = "[Delete]";
+    deleteButton.href = "#";
+
+    deleteButton.addEventListener("click", (e) => {
+        e.currentTarget.parentElement.remove();
+    });
+    liElement.append(deleteButton);
+    inputEl.value = "";
 }

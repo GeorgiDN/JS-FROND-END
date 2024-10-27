79644ed4179257aa878cec633f28a20d0d97e171
GeorgiDN <go6odn28@gmail.com>
2024-10-27 16:51:56 +0200

    added more solutions

--- JAVASCRIPT/06-domManipulationEvents/Lab/02. Delete-From-Table/app.js
+++ JAVASCRIPT/06-domManipulationEvents/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const customerTableEl = document.getElementById("customers");
+    const inputEl = document.querySelector("input[type=text][name=email]");
+    const resultEl = document.getElementById("result");
+    const searchedEmail = inputEl.value;
+    const tdElements = customerTableEl.querySelectorAll("tbody td:last-child");
+    const searchedEl = Array.from(tdElements).find(el => el.textContent === searchedEmail);
+
+    if (searchedEl) {
+        searchedEl.parentElement.remove();
+        resultEl.textContent = "Deleted";
+    } else {
+        resultEl.textContent = "Not found.";
+    }
+    inputEl.value = "";
 }

6a5d1c36ba093c84ae4ddc922e92833d481c35c8
GeorgiDN <go6odn28@gmail.com>
2024-10-29 12:19:12 +0200

    added more solutions

--- JAVASCRIPT/06.Dom-Manipulation-Events/Exercise/05.Encode-and-Decode-Messages/app.js
+++ JAVASCRIPT/06.Dom-Manipulation-Events/Exercise/05.Encode-and-Decode-Messages/app.js
@@ -1,3 +1,63 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let [encodeArea, decodeArea] = Array.from(document.querySelectorAll("textarea"));
+    const [encodeButton, decodeButton] = Array.from(document.querySelectorAll("button"));
+
+    encodeButton.addEventListener("click", () => {
+        let currentMessage = encodeArea.value;
+        let encodedMessage = "";
+        for (let char of currentMessage) {
+            encodedMessage += String.fromCharCode(char.charCodeAt(0) + 1);
+        }
+        decodeArea.value = encodedMessage;
+        encodeArea.value = "";
+    });
+
+    decodeButton.addEventListener("click", () => {
+        let currentMessage = decodeArea.value;
+        let decodedMessage = "";
+        for (let char of currentMessage) {
+            decodedMessage += String.fromCharCode(char.charCodeAt(0) - 1);
+        }
+        decodeArea.value = decodedMessage;
+    });
 }
+
+
+
+
+
+
+
+
+// //  0/100
+// function encodeAndDecodeMessages() {
+//     const divs = document.querySelectorAll("main > div");
+//
+//     const encodeBox = divs[0];
+//     const decodeBox = divs[1];
+//
+//     const messageArea = encodeBox.querySelector("textarea");
+//     const encodeButton = encodeBox.querySelector("button");
+//
+//     const lastMessageArea = decodeBox.querySelector("textarea");
+//     const decodeButton = decodeBox.querySelector("button");
+//
+//     encodeButton.addEventListener("click", () => {
+//         let currentMessage = messageArea.value;
+//         let encodedMessage = "";
+//         for (let char of currentMessage) {
+//             encodedMessage += String.fromCharCode(char.charCodeAt(0) + 1);
+//         }
+//         lastMessageArea.value = encodedMessage;
+//         messageArea.value = "";
+//     });
+//
+//     decodeButton.addEventListener("click", () => {
+//         let currentMessage = lastMessageArea.value;
+//         let decodedMessage = "";
+//         for (let char of currentMessage) {
+//             decodedMessage += String.fromCharCode(char.charCodeAt(0) - 1);
+//         }
+//         lastMessageArea.value = decodedMessage;
+//     });
+// }

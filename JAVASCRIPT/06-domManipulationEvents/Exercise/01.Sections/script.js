3ce1bad12dd1883d4818eb2a886cb8997cd7cc09
GeorgiDN <go6odn28@gmail.com>
2024-10-29 07:33:50 +0200

    added more solutions

---
+++ JAVASCRIPT/06-domManipulationEvents/Exercise/01.Sections/script.js
@@ -0,0 +1,42 @@
+function create(words) {
+    const contentDivEl = document.getElementById("content");
+
+    function createWordStructure(word) {
+        const pEl = document.createElement("p");
+        const divEl = document.createElement("div");
+        pEl.textContent = word;
+        pEl.style.display = "none";
+        divEl.appendChild(pEl);
+        return divEl;
+    }
+
+    for (let word of words) {
+        const divEl = createWordStructure(word);
+        divEl.addEventListener("click", (event) => {
+            event.target.querySelector("p").style.display = "block";
+
+    });
+        contentDivEl.appendChild(divEl);
+}}
+
+
+
+
+
+// function create(words) {
+//     const contentDivEl = document.getElementById("content");
+//
+//     words.forEach((word) => {
+//         const pEl = document.createElement("p");
+//         const divEl = document.createElement("div");
+//
+//         pEl.textContent = word;
+//         pEl.style.display = "none";
+//         divEl.appendChild(pEl);
+//
+//         divEl.addEventListener("click", (event) => {
+//             event.target.querySelector("p").style.display = "block";
+//         });
+//         contentDivEl.appendChild(divEl);
+//     });
+// }

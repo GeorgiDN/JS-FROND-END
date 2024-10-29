b0d7c151dede2ef78bf7d17673aedfdf7f2eb09e
GeorgiDN <go6odn28@gmail.com>
2024-10-29 10:30:17 +0200

    added more solutions

--- JAVASCRIPT/06.Dom-Manipulation-Events/Exercise/04.Fill-Dropdown/dropdown.js
+++ JAVASCRIPT/06.Dom-Manipulation-Events/Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const itemText = document.getElementById("newItemText");
+    const itemValue = document.getElementById("newItemValue");
+    const dopDownMenu = document.getElementById("menu");
+    const optionEl = document.createElement("option");
+    optionEl.textContent = itemText.value;
+    optionEl.value = itemValue.value;
+    dopDownMenu.appendChild(optionEl);
+    itemText.value = "";
+    itemValue.value = "";
 }

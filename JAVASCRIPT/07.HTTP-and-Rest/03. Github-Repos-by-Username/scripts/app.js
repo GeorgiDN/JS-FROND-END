728785688f8c2441d403d42447686d4202e0f88d
GeorgiDN <go6odn28@gmail.com>
2024-11-02 17:39:51 +0200

    added more solutions

--- JAVASCRIPT/07.HTTP-and-Rest/03. Github-Repos-by-Username/scripts/app.js
+++ JAVASCRIPT/07.HTTP-and-Rest/03. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,33 @@
+
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById("username").value
+	const reposList = document.getElementById("repos");
+	reposList.innerHTML = "";
+
+	const url = `https://api.github.com/users/${username}/repos`;
+	fetch(url)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("User not found or unable to load repos");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			data.forEach((repo) => {
+				const listItem = document.createElement("li");
+				const link = document.createElement("a");
+
+				link.href = repo.html_url;
+				link.textContent = repo.full_name;
+				link.target = "_blank";
+
+				listItem.appendChild(link);
+				reposList.appendChild(listItem);
+			});
+		})
+		.catch((error) => {
+			const errorItem = document.createElement("li");
+			errorItem.textContent = "User not found or unable to load repos.";
+			reposList.appendChild(errorItem);
+		});
 }
--- JAVASCRIPT/07.HTTP-and-Rest/04. Github-Commits/app.js
+++ JAVASCRIPT/07.HTTP-and-Rest/04. Github-Commits/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById("username").value;
+    const repository = document.getElementById("repo").value;
+    const unorderedList = document.getElementById("commits");
+
+    unorderedList.innerHTML = "";
+    const url = `https://api.github.com/repos/${username}/${repository}/commits`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} (Not Found)`);
+            }
+            return response.json();
+        })
+        .then(commits => {
+            for (const commit of commits) {
+                const listItem = document.createElement("li");
+                listItem.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+                unorderedList.appendChild(listItem);
+            }
+        })
+        .catch(error => {
+            const liItem = document.createElement("li");
+            listItem.textContent = error.message;
+            unorderedList.appendChild(listItem);
+        });
 }

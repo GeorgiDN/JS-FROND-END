e585592cb2a0014eeb828a61f3ef117e7d01bab5
GeorgiDN <go6odn28@gmail.com>
2024-11-02 08:41:31 +0200

    added more solutions

--- JAVASCRIPT/07.HTTP-and-Rest/02. GitHub-Repos/scripts/app.js
+++ JAVASCRIPT/07.HTTP-and-Rest/02. GitHub-Repos/scripts/app.js
@@ -1,3 +1,14 @@
 function loadRepos() {
-   console.log("TODO...");
+   const url = "https://api.github.com/users/testnakov/repos";
+   const resultElement = document.getElementById("res");
+
+   fetch(url)
+       .then(response => response.text())
+       .then(data => {
+          resultElement.textContent = data;
+       })
+       .catch(error => {
+          console.error("Error fetching the repositories", error);
+          resultElement.textContent = "Error loading repositories";
+       });
 }

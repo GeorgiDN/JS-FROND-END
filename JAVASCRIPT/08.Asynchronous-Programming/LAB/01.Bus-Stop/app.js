b73cb46cb7667d3b9778c261e784bc8e1efb24e2
GeorgiDN <go6odn28@gmail.com>
2024-11-03 15:09:03 +0200

    added more solutions

--- JAVASCRIPT/08.Asynchronous-Programming/LAB/01.Bus-Stop/app.js
+++ JAVASCRIPT/08.Asynchronous-Programming/LAB/01.Bus-Stop/app.js
@@ -1,3 +1,48 @@
 function getInfo() {
-    // TODO:
+    const baseUrl = "http://localhost:3030/jsonstore/bus/businfo";
+    const stopElement = document.getElementById("stopId");
+    const stopNameElement = document.getElementById("stopName");
+    const busesElement = document.getElementById("buses");
+
+    const stopId = stopElement.value;
+    fetch(`${baseUrl}/${stopId}`)
+        .then(res => res.json())
+        .then(data => {
+            stopNameElement.textContent = data.name
+
+            for (const bussId in data.buses) {
+                const liElement = document.createElement("li");
+                liElement.textContent = `Bus ${bussId} arrives in ${data.buses[bussId]} minutes`;
+                busesElement.appendChild(liElement);
+            }
+        })
+    .catch(() => {
+        stopNameElement.textContent = "Error"
+    })
 }
+
+
+
+
+
+
+// async function getInfo() {
+//     const desiredID = document.querySelector("#stopId").value;
+//     const resultTitle = document.querySelector("#stopName");
+//     const resultList = document.querySelector("#buses");
+//     resultList.innerHTML = "";
+//
+//     try {
+//         response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${desiredID}`);
+//         parsedData = await response.json();
+//
+//         resultTitle.textContent = parsedData.name
+//         Object.entries(parsedData.buses).forEach(([busId, time]) => {
+//             let listItem = document.createElement("li")
+//             listItem.textContent = `Bus ${busId} arrives in ${time} minutes`
+//             resultList.appendChild(listItem)
+//         })
+//     } catch (_) {
+//         resultTitle.textContent = "Error"
+//     }
+// }
--- JAVASCRIPT/08.Asynchronous-Programming/LAB/02.Bus-Schedule/app.js
+++ JAVASCRIPT/08.Asynchronous-Programming/LAB/02.Bus-Schedule/app.js
@@ -1,10 +1,38 @@
 function solve() {
-    function depart() {
-        // TODO: 
+    const textBox = document.querySelector("#info span");
+    const departBtn = document.getElementById("depart");
+    const arriveBtn = document.getElementById("arrive");
+    const baseUrl = "http://localhost:3030/jsonstore/bus/schedule";
+
+    let currentStop = {
+        id: "depot",
+        name: "Not Connected"
+    };
+
+    async function depart() {
+        try {
+            const response = await fetch(`${baseUrl}/${currentStop.id}`);
+            const data = await response.json();
+
+            currentStop = data;
+            console.log(currentStop)
+            textBox.textContent = `Next stop ${currentStop.name}`
+
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            textBox.textContent = "Error";
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
     }
 
-    async function arrive() {
-        // TODO:
+    function arrive() {
+        textBox.textContent = `Arriving at ${currentStop.name}`;
+
+        currentStop.id = currentStop.next;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {

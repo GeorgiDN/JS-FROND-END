70b6694970f152f5ea68fb8dd4c6e456dae6de31
GeorgiDN <go6odn28@gmail.com>
2024-11-04 08:25:17 +0200

    added more solutions

--- JAVASCRIPT/09.HTTP-and-AJAX-Exercise/02.Messenger/app.js
+++ JAVASCRIPT/09.HTTP-and-AJAX-Exercise/02.Messenger/app.js
@@ -1,5 +1,43 @@
 function attachEvents() {
-    // TODO:
+    const textAreaElement = document.getElementById("messages");
+    const nameInputElement = document.querySelector("#main #controls div:first-of-type input");
+    const messageInputElement = document.querySelector("#main #controls div:last-of-type input");
+    const baseUrl = "http://localhost:3030/jsonstore/messenger";
+    const submitBtnElement = document.getElementById("submit");
+    const refreshBtnElement = document.getElementById("refresh");
+
+    let messageData = {};
+
+    submitBtnElement.addEventListener("click", () => {
+        messageData = sendMessage(nameInputElement.value, messageInputElement.value)
+        fetch(baseUrl, {
+            method: "POST",
+            body: JSON.stringify(messageData)
+        });
+    });
+
+    refreshBtnElement.addEventListener("click", refreshEvent)
+
+   function sendMessage(author, content) {
+        return { author, content };
+    }
+
+    async function refreshEvent (event) {
+        textAreaElement.textContent = "";
+
+        let messageResponse = await fetch(baseUrl);
+        let messages = await messageResponse.json();
+
+        let resultArray = [];
+
+        Object.values(messages)
+            .forEach(m => {
+                let author = m.author;
+                let content = m.content;
+                resultArray.push(`${author}: ${content}`);
+            })
+        textAreaElement.textContent = resultArray.join("\n");
+    }
 }
 
 attachEvents();

ade3382def1476d0c73a8677bea580538068f3f4
GeorgiDN <go6odn28@gmail.com>
2024-11-05 07:34:26 +0200

    added more solutions

--- JAVASCRIPT/09.HTTP-and-AJAX-Exercise/04.Students/app.js
+++ JAVASCRIPT/09.HTTP-and-AJAX-Exercise/04.Students/app.js
@@ -1,14 +1,13 @@
 function attachEvents() {
-    const baseUrl = 'http://localhost:3030/jsonstore/collections/students';
+    const baseUrl = "http://localhost:3030/jsonstore/collections/students";
+    const studentsTbodyElement = document.querySelector("#results tbody");
+    const submitElement = document.getElementById("submit");
 
-    const studentsTbodyElement = document.querySelector('#results tbody');
-    const submitElement = document.getElementById('submit');
-
-    submitElement.addEventListener('click', () => {
-        const firstNameElement = document.querySelector('input[name=firstName]');
-        const lastNameElement = document.querySelector('input[name=lastName]');
-        const facultyNumberElement = document.querySelector('input[name=facultyNumber]');
-        const gradeElement = document.querySelector('input[name=grade]');
+    submitElement.addEventListener("click", async () => {
+        const firstNameElement = document.querySelector("input[name=firstName]");
+        const lastNameElement = document.querySelector("input[name=lastName]");
+        const facultyNumberElement = document.querySelector("input[name=facultyNumber]");
+        const gradeElement = document.querySelector("input[name=grade]");
 
         const newStudent = {
             firstName: firstNameElement.value,
@@ -17,51 +16,63 @@ function attachEvents() {
             grade: gradeElement.value,
         };
 
-        fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(newStudent),
-        })
-            .then(res => res.json())
-            .then(data => {
-                studentsTbodyElement.appendChild(createStudentElement(data))
-
-                firstNameElement.value = '';
-                lastNameElement.value = '';
-                facultyNumberElement.value = '';
-                gradeElement.value = '';
+        try {
+            const response = await fetch(baseUrl, {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify(newStudent),
             });
+
+            const data = await response.json();
+            studentsTbodyElement.appendChild(createStudentElement(data));
+
+            firstNameElement.value = "";
+            lastNameElement.value = "";
+            facultyNumberElement.value = "";
+            gradeElement.value = "";
+
+        } catch (error) {
+            console.error("Error creating student:", error);
+        }
     });
 
-    fetch(baseUrl)
-        .then(res => res.json())
-        .then(data =>
-            Object.values(data)
-                .forEach(student => studentsTbodyElement.appendChild(createStudentElement(student)))
-        )
+    async function loadStudents() {
+        try {
+            const response = await fetch(baseUrl);
+            const data = await response.json();
+
+            Object.values(data).forEach(student =>
+                studentsTbodyElement.appendChild(createStudentElement(student))
+            );
+        } catch (error) {
+            console.error("Error loading students:", error);
+        }
+    }
 
     function createStudentElement(student) {
-        const trElement = document.createElement('tr');
+        const trElement = document.createElement("tr");
 
-        const createRd = value => {
-            const tdElement = document.createElement('td');
+        const createTd = value => {
+            const tdElement = document.createElement("td");
             tdElement.textContent = value;
-
             return tdElement;
         };
 
-        trElement.appendChild(createRd(student.firstName));
-        trElement.appendChild(createRd(student.lastName));
-        trElement.appendChild(createRd(student.facultyNumber));
-        trElement.appendChild(createRd(student.grade));
+        trElement.appendChild(createTd(student.firstName));
+        trElement.appendChild(createTd(student.lastName));
+        trElement.appendChild(createTd(student.facultyNumber));
+        trElement.appendChild(createTd(student.grade));
 
         return trElement;
     }
+
+    loadStudents();
 }
 
 attachEvents();
+
 
 
 

e3ec5ab3c2dfa4aa64404e5a948a08c58086ef88
GeorgiDN <go6odn28@gmail.com>
2024-11-19 22:17:56 +0200

    dom exercise: task - table-search-engine update

--- JAVASCRIPT/05.Dom-Introduction/Exercise/05.Table-Search-Engine/solution.js
+++ JAVASCRIPT/05.Dom-Introduction/Exercise/05.Table-Search-Engine/solution.js
@@ -1,29 +1,50 @@
 function solve() {
-    document.querySelector('#searchBtn').addEventListener('click', onClick);
+    const searchString = document.querySelector("#searchField").value.toLowerCase().trim();
+    const students = document.querySelectorAll("table tbody tr");
 
-    function onClick() {
-        const tableRowEls = document.querySelectorAll("table.container tbody tr");
-        const searchedText = document.getElementById("searchField").value.toLowerCase();
-
-        function removeClassList(tableRowEls) {
-            tableRowEls.forEach((row) => {
-                row.classList.remove("select");
-            });
-        }
-
-        function addClassList(tableRowEls) {
-            tableRowEls.forEach((row) => {
-                if (searchedText.length > 0 && row.textContent.toLowerCase().includes(searchedText)) {
-                    row.classList.add("select");
-                }
-            });
-        }
-
-        removeClassList(tableRowEls);
-        addClassList(tableRowEls);
-        document.getElementById("searchField").value = "";
+    if (searchString === "") {
+        return;
     }
+
+    students.forEach(student => {
+        student.classList.remove("select");
+
+        if (student.textContent.toLowerCase().includes(searchString)) {
+            student.classList.add("select")
+        }
+    });
 }
+
+
+
+
+
+// function solve() {
+//     document.querySelector('#searchBtn').addEventListener('click', onClick);
+//
+//     function onClick() {
+//         const tableRowEls = document.querySelectorAll("table.container tbody tr");
+//         const searchedText = document.getElementById("searchField").value.toLowerCase();
+//
+//         function removeClassList(tableRowEls) {
+//             tableRowEls.forEach((row) => {
+//                 row.classList.remove("select");
+//             });
+//         }
+//
+//         function addClassList(tableRowEls) {
+//             tableRowEls.forEach((row) => {
+//                 if (searchedText.length > 0 && row.textContent.toLowerCase().includes(searchedText)) {
+//                     row.classList.add("select");
+//                 }
+//             });
+//         }
+//
+//         removeClassList(tableRowEls);
+//         addClassList(tableRowEls);
+//         document.getElementById("searchField").value = "";
+//     }
+// }
 
 
 // function solve() {

928b5b80f3342c570f0c19a7b142eb3eef3d4d42
GeorgiDN <go6odn28@gmail.com>
2024-11-19 20:06:57 +0200

    dom exercise: task - pascal or camelcase update

--- JAVASCRIPT/05.Dom-Introduction/Exercise/02.Pascal-or-Camel-Case/solution.js
+++ JAVASCRIPT/05.Dom-Introduction/Exercise/02.Pascal-or-Camel-Case/solution.js
@@ -1,23 +1,53 @@
-function solve () {
-    const validNamingConventions = ["Camel Case", "Pascal Case"];
-    const textInputEl = document.querySelector("#text");
-    const namingConventionEl = document.querySelector("#naming-convention");
-    const resultEl = document.querySelector(".result-container #result");
+function solve() {
+    const validConventions = ["Camel Case", "Pascal Case"];
+    const textInput = document.querySelector("#text").value.toLowerCase();
+    const namingConvention = document.querySelector("#naming-convention").value;
+    const resultSpanEl = document.querySelector(".result-container span");
+    let output = "";
 
-    if (!validNamingConventions.includes(namingConventionEl.value)) {
-        resultEl.textContent = "Error!";
-        return;
+    if (!validConventions.includes(namingConvention)) {
+        output = "Error!"
+
+    } else {
+        const wordsArr = textInput.split(" ");
+        if (namingConvention === "Camel Case") {
+            output += wordsArr.shift().toLowerCase();
+        }
+        output = textToUpperCase(wordsArr);
     }
 
-    const words = textInputEl.value.toLowerCase().split(" ");
-    let result = "";
+    resultSpanEl.textContent = output;
 
-    if (namingConventionEl.value === "Camel Case") {
-        result = words[0] + words.slice(1)
-            .map(word => word[0].toUpperCase() + word.slice(1))
-            .join("")
-    } else if (namingConventionEl.value === "Pascal Case") {
-        result = words.map(word => word[0].toUpperCase() + word.slice(1)).join("");
+    function textToUpperCase(wordsArr) {
+        wordsArr.forEach((word) => {
+            output += word[0].toUpperCase() + word.slice(1);
+        });
+        return output;
     }
-    resultEl.textContent = result;
 }
+
+
+
+// function solve () {
+//     const validNamingConventions = ["Camel Case", "Pascal Case"];
+//     const textInputEl = document.querySelector("#text");
+//     const namingConventionEl = document.querySelector("#naming-convention");
+//     const resultEl = document.querySelector(".result-container #result");
+//
+//     if (!validNamingConventions.includes(namingConventionEl.value)) {
+//         resultEl.textContent = "Error!";
+//         return;
+//     }
+//
+//     const words = textInputEl.value.toLowerCase().split(" ");
+//     let result = "";
+//
+//     if (namingConventionEl.value === "Camel Case") {
+//         result = words[0] + words.slice(1)
+//             .map(word => word[0].toUpperCase() + word.slice(1))
+//             .join("")
+//     } else if (namingConventionEl.value === "Pascal Case") {
+//         result = words.map(word => word[0].toUpperCase() + word.slice(1)).join("");
+//     }
+//     resultEl.textContent = result;
+// }

cbbb9f7fe288889d9a4bbc98587f7c28a71a180e
GeorgiDN <go6odn28@gmail.com>
2024-11-21 19:41:25 +0200

    update task: delete from table

--- JAVASCRIPT/06.Dom-Manipulation-Events/Lab/02. Delete-From-Table/app.js
+++ JAVASCRIPT/06.Dom-Manipulation-Events/Lab/02. Delete-From-Table/app.js
@@ -1,16 +1,38 @@
 function deleteByEmail() {
-    const customerTableEl = document.getElementById("customers");
-    const inputEl = document.querySelector("input[type=text][name=email]");
-    const resultEl = document.getElementById("result");
-    const searchedEmail = inputEl.value;
-    const tdElements = customerTableEl.querySelectorAll("tbody td:last-child");
-    const searchedEl = Array.from(tdElements).find(el => el.textContent === searchedEmail);
+    const searchStr = document.querySelector('input[name="email"]').value.toLowerCase();
+    const people = document.querySelectorAll('table tbody tr td:nth-child(2)');
+    const resultEl = document.querySelector('#result');
+    let found = false;
 
-    if (searchedEl) {
-        searchedEl.parentElement.remove();
-        resultEl.textContent = "Deleted";
-    } else {
-        resultEl.textContent = "Not found.";
+    for (let person of people) {
+        if (person.textContent.toLowerCase() === searchStr) {
+            resultEl.textContent = 'Deleted.';
+            person.parentElement.remove();
+            found = true;
+            break;
+        }
     }
-    inputEl.value = "";
+
+    if (!found) {
+        resultEl.textContent = 'Not found.';
+    }
 }
+
+
+
+// function deleteByEmail() {
+//     const customerTableEl = document.getElementById("customers");
+//     const inputEl = document.querySelector("input[type=text][name=email]");
+//     const resultEl = document.getElementById("result");
+//     const searchedEmail = inputEl.value;
+//     const tdElements = customerTableEl.querySelectorAll("tbody td:last-child");
+//     const searchedEl = Array.from(tdElements).find(el => el.textContent === searchedEmail);
+//
+//     if (searchedEl) {
+//         searchedEl.parentElement.remove();
+//         resultEl.textContent = "Deleted";
+//     } else {
+//         resultEl.textContent = "Not found.";
+//     }
+//     inputEl.value = "";
+// }

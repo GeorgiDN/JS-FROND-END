a47902cc909eeb089399e8f745bf8889796615a4
GeorgiDN <go6odn28@gmail.com>
2024-11-25 22:08:34 +0200

    task: locked profile

--- JAVASCRIPT/06.Dom-Manipulation-Events/Exercise2/03. Locked-Profile/locked-profile.js
+++ JAVASCRIPT/06.Dom-Manipulation-Events/Exercise2/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const profiles = document.querySelectorAll(".profile");
+
+    profiles.forEach(profile => {
+        const lockRadio = profile.querySelector("input[type='radio'][id*='Lock']");
+        const unlockRadio = profile.querySelector("input[type='radio'][id*='Unlock']");
+        const showButton = profile.querySelector("button");
+        const hiddenField = profile.querySelector(".hidden-fields");
+
+        showButton.addEventListener("click", () => {
+            if (unlockRadio.checked) {
+                if (hiddenField.style.display === "block") {
+                    hiddenField.style.display = "none";
+                    showButton.textContent = "Show more";
+                } else {
+                    hiddenField.style.display = "block";
+                    showButton.textContent = "Hide it";
+                }
+            }
+        });
+    });
 }

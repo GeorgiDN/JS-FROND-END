8600f93908aafaf0f392415156d059542ea536c1
GeorgiDN <go6odn28@gmail.com>
2024-11-25 21:19:53 +0200

    task: fill dropdown

--- JAVASCRIPT/06.Dom-Manipulation-Events/Exercise2/04. Fill-Dropdown/fill-dropdown.js
+++ JAVASCRIPT/06.Dom-Manipulation-Events/Exercise2/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const nextItemInput = document.getElementById("newItemText");
+    const nextValueInput = document.getElementById("newItemValue");
+    const addButton = document.querySelector("input[type='submit']")
+    const selectMenu = document.getElementById("menu")
+
+    addButton.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const optionElement = document.createElement("option");
+        optionElement.textContent = nextItemInput.value;
+        optionElement.value = nextValueInput.value;
+        selectMenu.appendChild(optionElement);
+
+        nextItemInput.value = "";
+        nextValueInput.value = "";
+    })
 }

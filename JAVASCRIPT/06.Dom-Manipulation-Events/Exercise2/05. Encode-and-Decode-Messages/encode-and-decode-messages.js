684144b858535daf74505d8a3d8aeef04e5cba87
GeorgiDN <go6odn28@gmail.com>
2024-11-26 08:04:11 +0200

    add solution to task encode and decode messsage

--- JAVASCRIPT/06.Dom-Manipulation-Events/Exercise2/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ JAVASCRIPT/06.Dom-Manipulation-Events/Exercise2/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const message = document.querySelector("#encode textarea");
+    const encodeButton = document.querySelector("#encode button");
+    const lastReceivedMessage = document.querySelector("#decode textarea")
+    const decodeButton = document.querySelector("#decode button");
+    let encodedMessage = "";
+    let decodedMessage = "";
+    encodeButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (!message.value) {
+            return;
+        }
+        message.value.split("").forEach((ch) => {
+            encodedMessage += String.fromCharCode(ch.charCodeAt(0) + 1);
+        });
+        message.value = "";
+        lastReceivedMessage.value = encodedMessage;
+        encodedMessage = "";
+    });
+
+    decodeButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (!lastReceivedMessage.value) {
+            return;
+        }
+        lastReceivedMessage.value.split("").forEach((ch) => {
+            decodedMessage += String.fromCharCode(ch.charCodeAt(0) - 1);
+        });
+    lastReceivedMessage.value = "";
+    lastReceivedMessage.value = decodedMessage;
+    decodedMessage = "";
+    });
 }
+

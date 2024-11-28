01c89e5da58ba6bbd22fc863519851709cfd2710
GeorgiDN <go6odn28@gmail.com>
2024-11-28 08:02:36 +0200

    add solution github-repos

--- JAVASCRIPT/07.HTTP-and-Rest/02. GitHub-Repos/scripts/app.js
+++ JAVASCRIPT/07.HTTP-and-Rest/02. GitHub-Repos/scripts/app.js
@@ -1,14 +1,32 @@
-function loadRepos() {
-   const url = "https://api.github.com/users/testnakov/repos";
-   const resultElement = document.getElementById("res");
+async function loadRepos() {
+    const url = "https://api.github.com/users/testnakov/repos";
+    const resultElement = document.getElementById("res");
 
-   fetch(url)
-       .then(response => response.text())
-       .then(data => {
-          resultElement.textContent = data;
-       })
-       .catch(error => {
-          console.error("Error fetching the repositories", error);
-          resultElement.textContent = "Error loading repositories";
-       });
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        resultElement.textContent = data;
+    } catch (error) {
+        console.error("Error fetching the repositories", error);
+        resultElement.textContent = "Error loading repositories";
+    }
 }
+
+
+// function loadRepos() {
+//    const url = "https://api.github.com/users/testnakov/repos";
+//    const resultElement = document.getElementById("res");
+//
+//
+//
+//    fetch(url)
+//        .then(response => response.text())
+//        .then(data => {
+//           resultElement.textContent = data;
+//        })
+//        .catch(error => {
+//           console.error("Error fetching the repositories", error);
+//           resultElement.textContent = "Error loading repositories";
+//        });
+//
+// }

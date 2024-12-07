4134ff67d72c7362e71b832b8d034409c0fbdf8c
GeorgiDN <go6odn28@gmail.com>
2024-12-07 18:07:25 +0200

    task: locked-profile

--- JAVASCRIPT/09.HTTP-and-AJAX-Exercise/05. Locked-Profile/app.js
+++ JAVASCRIPT/09.HTTP-and-AJAX-Exercise/05. Locked-Profile/app.js
@@ -1,56 +1,142 @@
-async function lockedProfile() {
-    const baseURL = "http://localhost:3030/jsonstore/advanced/profiles";
-
-    let profilesResponse = await fetch(baseURL);
-    let profiles = await profilesResponse.json();
-
-    let mainElement = document.getElementById("main");
-    mainElement.innerHTML = "";
-
+function lockedProfile() {
+    const baseUrl = "http://localhost:3030/jsonstore/advanced/profiles";
+    const mainElement = takeElementByTag("#main");
     let counter = 1;
-    for (let [profileID, profileInfo] of Object.entries(profiles)) {
-        let divProfile = document.createElement("div");
-        divProfile.className = "profile"
 
-        let profileHTML = `
-            <img src="./iconProfile2.png" class="userIcon" />
-            <label>Lock</label>
-            <input type="radio" name="user${counter}Locked" value="lock" checked>
-            <label>Unlock</label>
-            <input type="radio" name="user${counter}Locked" value="unlock"><br>
-            <hr>
-            <label>Username</label>
-            <input type="text" name="user${counter}Username" value="${profileInfo.username}" disabled readonly />
-            <div class="user${counter}HiddenFields">
-                <hr>
-                <label>Email:</label>
-                <input type="email" name="user${counter}Email" value="${profileInfo.email}" disabled readonly />
-                <label>Age:</label>
-                <input type="email" name="user${counter}Age" value="${profileInfo.age}" disabled readonly />
-            </div>
-            
-            <button>Show more</button>
-        `
+    async function loadProfiles() {
+        const response = await fetch(baseUrl);
+        const result = await response.json();
+        const profiles = Object.values(result);
 
-        divProfile.innerHTML = profileHTML;
-        divProfile.querySelector("div").style.display = "none";
-        mainElement.appendChild(divProfile);
+        mainElement.innerHTML = "";
 
-        let showMoreButton = divProfile.querySelector("button");
-        showMoreButton.addEventListener("click", moreInfoEvent);
+        const recordsElements = profiles.map(profile =>
+            createRecord(profile.username, profile.email, profile.age, profile._id));
 
-        function moreInfoEvent(event) {
-            let lockRadioButton = divProfile.querySelector("input[type='radio']")
+        mainElement.append(...recordsElements);
+    }
 
-            if (showMoreButton.textContent === "Show more" && !lockRadioButton.checked) {
-                divProfile.querySelector("div").style.display = "block"
-                showMoreButton.textContent = "Hide it"
-            } else if (showMoreButton.textContent === "Hide it" && !lockRadioButton.checked) {
-                divProfile.querySelector("div").style.display = "none"
-                showMoreButton.textContent = "Show more"
+    function createRecord(username, email, age, profileId) {
+        let currentCounter = counter++;
+
+        const divProfile = createEl("div");
+        divProfile.classList.add("profile");
+
+        const imgElement = createEl("img");
+        imgElement.setAttribute("src", "./iconProfile2.png");
+        imgElement.classList.add("userIcon");
+
+        const lockLabelElement = createEl("label");
+        lockLabelElement.textContent = "Lock";
+
+        const InputRadioLockElement = createEl("input");
+        InputRadioLockElement.type = "radio";
+        InputRadioLockElement.name = `user${currentCounter}Locked`;
+        InputRadioLockElement.value = "lock";
+        InputRadioLockElement.checked = true;
+
+        const unLockLabelElement = createEl("label");
+        unLockLabelElement.textContent = "Unlock";
+
+        const InputRadioUnLockElement = createEl("input");
+        InputRadioUnLockElement.type = "radio";
+        InputRadioUnLockElement.name = `user${currentCounter}Locked`;
+        InputRadioUnLockElement.value = "unlock";
+        InputRadioUnLockElement.checked = false;
+
+        const brElement = createEl("br");
+
+        const hrProfileElement = createEl("hr");
+
+        const usernameLabelElement = createEl("label");
+
+        const inputTextElement = createEl("input");
+        inputTextElement.type = "text";
+        inputTextElement.name = `user${currentCounter}Username`;
+        inputTextElement.value = username;
+        inputTextElement.disabled = true;
+        inputTextElement.readOnly = true;
+
+        // Hidden
+        const divHiddenFields = createEl("div");
+        divHiddenFields.id = `user${currentCounter}HiddenFields`;
+        divHiddenFields.style.display = "none";
+
+        const hrHiddenElement = createEl("hr");
+
+        const emailLabelElement = createEl("label");
+        emailLabelElement.textContent = "Email:";
+
+        const inputEmailElement = createEl("input");
+        inputEmailElement.type = "email";
+        inputEmailElement.name = `user${currentCounter}Email`;
+        inputEmailElement.value = email;
+        inputEmailElement.disabled = true;
+        inputEmailElement.readOnly = true;
+
+        const labelAgeElement = createEl("label");
+        labelAgeElement.textContent = "Age:"
+
+        const inputAgeElement = createEl("input");
+        inputAgeElement.type = "number";
+        inputAgeElement.name = `user${currentCounter}Age`;
+        inputAgeElement.value = age;
+        inputAgeElement.disabled = true;
+        inputAgeElement.readOnly = true;
+
+        const buttonShowMoreElement = createEl("button");
+        buttonShowMoreElement.textContent = "Show more";
+        buttonShowMoreElement.addEventListener("click", () => {
+            showMore(divHiddenFields, InputRadioLockElement, buttonShowMoreElement);
+        })
+
+        // Append hidden fields
+        divHiddenFields.appendChild(hrHiddenElement);
+        divHiddenFields.appendChild(emailLabelElement);
+        divHiddenFields.appendChild(inputEmailElement);
+        divHiddenFields.appendChild(labelAgeElement);
+        divHiddenFields.appendChild(inputAgeElement);
+
+        divProfile.appendChild(imgElement);
+        divProfile.appendChild(lockLabelElement);
+        divProfile.appendChild(InputRadioLockElement);
+        divProfile.appendChild(unLockLabelElement);
+        divProfile.appendChild(InputRadioUnLockElement);
+        divProfile.appendChild(brElement);
+        divProfile.appendChild(hrProfileElement);
+        divProfile.appendChild(usernameLabelElement);
+        divProfile.appendChild(inputTextElement);
+        divProfile.appendChild(divHiddenFields);
+        divProfile.appendChild(buttonShowMoreElement);
+
+        return divProfile;
+    }
+
+    function showMore(divHiddenFields, InputRadioLockElement, buttonShowMoreElement) {
+        if (InputRadioLockElement.checked === false) {
+            if (divHiddenFields.style.display === "none") {
+                divHiddenFields.style.display = "block";
+                buttonShowMoreElement.textContent = "Hide it";
+            } else {
+                divHiddenFields.style.display = "none";
+                buttonShowMoreElement.textContent = "Show more";
             }
         }
-
-        counter += 1
     }
+
+    function takeElementByTag(tag) {
+        return document.querySelector(tag);
+    }
+
+    function createEl(tag) {
+        return document.createElement(tag);
+    }
+
+    function clearInputs(...fields) {
+        for (let field of fields) {
+            field.value = "";
+        }
+    }
+
+    loadProfiles();
 }
